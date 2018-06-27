c66479185c7eef78c1b566d7c5d2d3eb16f3f587
Nikhil Lahoti <lahotinikhil92@gmail.com>
2018-06-26 23:25:40 -0700

    Isomorphic Strings

---
+++ IsomorphicStrings.py
@@ -0,0 +1,27 @@
+def isIsomorphic(s, t):
+
+    if len(s) != len(t): return False
+
+    dict = {}
+    val = set()
+    nStr = ""
+    for i in range(0, len(s)):
+        ch = s[i]
+
+        if ch in dict:
+            ch = dict[ch]
+        else:
+            if ch == t[i]:
+                dict[ch] = ch
+            else:
+                dict[ch] = t[i]
+                ch = t[i]
+        nStr += ch
+
+    if len(set(dict.values())) != len(dict): return False
+
+    if nStr == t: return  True
+    return False
+
+
+print(isIsomorphic("bty", "bat"))
