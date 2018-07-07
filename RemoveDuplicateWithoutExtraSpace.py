d57d36100d166d1f00ad2c3e80983cc1b4cf90da
nikhil <lahotinikhil92@gmail.com>
2018-07-06 23:49:20 -0700

    Remove Duplicate Without Extra Space

---
+++ RemoveDuplicateWithoutExtraSpace.py
@@ -0,0 +1,24 @@
+
+
+def removeDuplicate(nums):
+    index = 1
+
+    def BinarySearch(target, right):
+        left = 0
+        while left <= right:
+            mid = (int)((left + right) / 2)
+            if nums[mid] == target: return True
+            if nums[mid] > target: right = mid - 1
+            else: left = mid + 1
+        return False
+
+    for i in range(len(nums)):
+        if BinarySearch(nums[i], index - 1):
+            nums[index] = nums[i]
+            index += 1
+
+    nums = nums[0:index]
+    return len(nums)
+
+nums = [1,1,2,2,4,5,6]
+print(removeDuplicate(nums))
