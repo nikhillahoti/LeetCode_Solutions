cf13ec81ea8be87183e009b17b381512a2104e06
nikhil <lahotinikhil92@gmail.com>
2018-07-26 00:06:26 -0700

    House Robbery

--- HouseRobbery.py
+++ HouseRobbery.py
@@ -1,17 +1,23 @@
 
 
 def rob(nums):
-    def getMax(index, sum):
-        if index >= len(nums): return sum
-        max1 = getMax(index + 2, sum + nums[index])
-        max2 = getMax(index+1, sum)
-        return max1 if max1 > max2 else max2
 
-    return getMax(0, 0)
+    if len(nums) <= 0: return 0
 
-nums = [1,2,3,1]
-nums = [7,1,2,9]
-nums = [2,4]
-nums = [1,3,1]
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+
+    if len(nums) > 1:
+        dp[1] = nums[1] if nums[1] > nums[0] else nums[0]
+
+    for i in range(2,len(nums)):
+        dp[i] = dp[i-1] if dp[i-1] >= (nums[i] + dp[i-2]) else (nums[i] + dp[i-2])
+
+    return dp[len(nums) - 1]
+
+#nums = [1,2,3,1]
+#nums = [7,1,2,9]
+#nums = [2,4]
+#nums = [1,3,1]
 nums = [183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]
 print(rob(nums))
